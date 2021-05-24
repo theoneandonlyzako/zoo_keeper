b6df2f208240387e7bb37d5613d6481e437474f9
Zak Stamps <theoneandonlyzako@gmail.com>
2021-05-23 20:20:15 -0700

    heroku push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
-
+const
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
